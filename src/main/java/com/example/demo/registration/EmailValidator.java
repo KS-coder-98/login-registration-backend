0c48080e3fe4148b99af929f60ebf573e14f50da
KS-coder-98 <krzysztofsekowski1998@gmail.com>
2022-11-21 15:17:38 +0100

    add regex for EmailValidator

--- src/main/java/com/example/demo/registration/EmailValidator.java
+++ src/main/java/com/example/demo/registration/EmailValidator.java
@@ -3,12 +3,17 @@ package com.example.demo.registration;
 import org.springframework.stereotype.Service;
 
 import java.util.function.Predicate;
+import java.util.regex.Pattern;
 
 @Service
 public class EmailValidator implements Predicate<String> {
+
+    private static final String REGEX_EMAIL = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
+
     @Override
-    public boolean test(String s) {
-//        TODO: Regex to validate email
-        return true;
+    public boolean test(String emailAddress) {
+        return Pattern.compile(REGEX_EMAIL)
+                .matcher(emailAddress)
+                .matches();
     }
 }
